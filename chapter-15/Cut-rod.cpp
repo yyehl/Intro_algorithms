9c1dc337d14de7b46f1e7e6b9f2e937674ed3c13
yyehl <yyehl@foxmail.com>
2017-07-01 08:46:33 +0800

    add

--- chapter-15/Cut-rod.cpp
+++ chapter-15/Cut-rod.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <utility>
 using namespace std;
 
 
@@ -69,6 +70,36 @@ int cut_pod_dp_down_to_top(int p[], int n)
 	return r[n];
 }
 
+// 不仅保存最优解，而且保存最优解的切割方案
+
+void cut_pod_dp_down_to_top_extern(int p[], int n)
+{
+	if (n == 0)  return ;
+	int r[n+1];
+	int s[n+1];
+
+	r[0] = 0;
+	s[0] = 0;
+	
+	for (int i = 1; i <= n; ++i)
+	{
+		int q = 0;
+		for (int j = 1; j <= i; ++j)
+		{
+			if (q < p[j-1] + r[i-j])
+			{
+				q = p[j-1] + r[i-j];
+				s[i] = j;
+			}
+		}
+		r[i] = q;
+	}
+	
+	for (int i = 0; i < n+1; ++i)
+		cout << r[i] << " ";  cout << endl;
+	for (int i = 0; i < n+1; ++i)
+		cout << s[i] << " ";  cout << endl;
+}
 
 int main()
 {
@@ -81,6 +112,7 @@ int main()
 	cout << cut_pod_dp_down_to_top(p,1) << " " << cut_pod_dp_down_to_top(p,2) << " " << cut_pod_dp_down_to_top(p,3) << " "
 		 << cut_pod_dp_down_to_top(p, 4) << endl;
 
+	cut_pod_dp_down_to_top_extern(p, 10);
 	return 0;
 }
 
